c82295ca1d9e0858c05b142ca24184aa0373573b
RiD_32 <rostokin.den@yandex.ru>
2018-01-09 12:31:42 +0300

    Installed Webpack Hot Module

--- client/components/App.js
+++ client/components/App.js
@@ -3,7 +3,10 @@ import React from 'react';
 export default class App extends React.Component {
   render() {
     return (
-      <h1>Hello from React!</h1>
+      <div>
+        <h1>Hello from React!</h1>
+        <h2>How are you?</h2>
+      </div>
     );
   }
 }
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,20 +1,33 @@
 import path from 'path';
+import webpack from 'webpack';
 
 export default {
-  entry: path.join(__dirname, 'client/index.js'),
+  devtool: 'eval-source-map',
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, 'client/index.js'),
+  ],
   resolve: {
     extensions: ['.js', '.jsx'],
   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'server'),
+    publicPath: '/',
   },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
   module: {
     loaders: [
       {
         test: /\.js$/,
         include: path.join(__dirname, 'client'),
-        loaders: ['babel-loader'],
+        loaders: ['react-hot-loader/webpack', 'babel-loader'],
+        exclude: /node_modules/,
       },
     ],
   },
